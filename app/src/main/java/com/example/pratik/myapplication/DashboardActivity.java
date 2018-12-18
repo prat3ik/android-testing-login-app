1effc428e7586feba1d093922412fdb3ca4497a9
Prat3ik <pratikquora@gmail.com>
2018-12-18 22:06:26 +0530

    Sample application for login and logout added

---
+++ app/src/main/java/com/example/pratik/myapplication/DashboardActivity.java
@@ -0,0 +1,27 @@
+package com.example.pratik.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_screen);
+
+        TextView logOutText = findViewById(R.id.logout_textview);
+        logOutText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+    }
+}
